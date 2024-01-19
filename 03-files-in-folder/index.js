63f4e5cdbf74a2d0b501c0d49fa5fad3ce400c2b
Vanyha010 <vanyha010@icloud.com>
2024-01-19 10:29:54 +0300

    feat: add task 03 solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { readdir, stat } = require('node:fs/promises');
+
+const source = path.resolve(__dirname, 'secret-folder');
+
+async function readFolder() {
+  try {
+    const files = await readdir(source, { withFileTypes: true });
+    for (const file of files) {
+      try {
+        const fileStat = await stat(path.join(source, file.name));
+        if (file.isFile()) {
+          console.log(
+            `${file.name.split('.')[0]} - ${path
+              .extname(file.name)
+              .replace('.', '')} - ${(fileStat.size / 1024).toFixed(2)}kB`,
+          );
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+readFolder();
