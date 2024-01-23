90d8db0a715ce00be7bc6dfe0cdcb94f04228d6a
Vanyha010 <vanyha010@icloud.com>
2024-01-23 18:40:51 +0300

    fix: change method of file writing in task 05

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,18 +1,19 @@
 const path = require('path');
-const { readdir, appendFile, open } = require('fs').promises;
+const fs = require('fs');
+const { readdir } = require('fs').promises;
 
 const inputFolder = path.resolve(__dirname, 'styles');
 const outputFile = path.join(__dirname, 'project-dist', 'bundle.css');
 
 async function mergeStyles() {
   const files = await readdir(inputFolder, { withFileTypes: true });
+  const outputStream = fs.createWriteStream(outputFile);
   files.forEach(async (file) => {
     if (file.name.endsWith('.css')) {
-      const fd = await open(path.join(inputFolder, file.name));
-      const stream = fd.createReadStream();
-      stream.on('data', async (data) => {
-        await appendFile(outputFile, data);
-      });
+      const inputStream = fs.createReadStream(
+        path.join(inputFolder, file.name),
+      );
+      await inputStream.pipe(outputStream);
     }
   });
 }
